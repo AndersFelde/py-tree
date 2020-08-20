ab92312c380435c7acb4a66da43574e087f5b387
Sander Godard <sander@godard.no>
2020-08-20 12:26:50 +0200

    Add files via upload

---
+++ fuckMK.py
@@ -0,0 +1,35 @@
+import ntpath
+import os
+
+
+class dir():
+
+    def searchDir(self):
+        dirChilds = []
+        # fileChilds = []
+        dirs = os.listdir(self.path)
+        for d in dirs:
+            if os.path.isdir(self.path + "/" + d):
+                newDir = dir(self.path + "/" + d, self.depth+1, self)
+               # print(newDir)
+                dirChilds.append(newDir)
+            else:
+                print(("--" * (self.depth+1)) + "/" + d)
+
+        # return [dirChilds, fileChilds]
+        return dirChilds
+
+    def __init__(self, path, depth, parent=None):
+        self.path = path
+        self.depth = depth
+        self.parent = parent
+        print(self.printSelf())
+        # childs = self.searchDir()
+        self.dirChilds = self.searchDir()
+        # self.fileChilds = childs[1]
+
+    def printSelf(self):
+        return ("--" * self.depth) + ntpath.basename(self.path)
+
+root = dir("/home/sGodard/Documents/", 0)
+
